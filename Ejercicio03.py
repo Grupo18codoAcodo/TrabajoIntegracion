77df1a661278180789db32fefa888bb176097480
joarchaga <ingjosephchacon@gmail.com>
2023-08-22 23:34:51 -0300

    Subiendo Ejercicio03,04,06 y 07

--- Ejercicio03.py
+++ Ejercicio03.py
@@ -2,33 +2,24 @@
 Escribir un programa que reciba una cadena de caracteres y devuelva un diccionario 
 con cada palabra que contiene y la cantidad de veces que aparece (frecuencia).
 '''
-print ('@'*40,  'Inicio', '@'*40,'\n')
 
-def creador_dict(cadena):
-  '''Recibe una cadena de caracteres y regresa un diccionario con las palabras 
-  (keys) y conteo (value)'''
+def hacer_diccionario(cadena):
   lista_1= cadena.split()
-  dict_1={}
-  for i in list_1:
-    if i in dict_1:
-      dict_1[i] +=1
+  dicc_1={}
+  for i in range (len(lista_1)):
+    if lista_1[i] in dicc_1:
+      dicc_1[lista_1[i]] +=1
     else:
-      dict_1[i]= 1
-  return dict_1
-
-def contador_dict(dictionario):
-  '''Recibe un diccionario y regresa una tupla: la palabra mas repetida y cuantas veces 
-  aparece'''
-  palabra_frecuente= ''
-  cantidad=0
-  for keys,values in dictionario.items():
-    if values > cantidad:
-      cantidad= values
-      palabra_frecuente= keys
-  return palabra_frecuente,cantidad
-entrada=input('Ingrese su cadena de caracteres: ')
-print(creador_dict(entrada))
-print(contador_dict(creador_dict(entrada)))
+      dicc_1[lista_1[i]]= 1
+  return dicc_1
 
 
+print ('@'*40,  'Inicio', '@'*40,'\n')
+continuar= ('s')
+while (continuar=='s' or continuar=='S'):
+
+  entrada=input('\n Ingrese su cadena de caracteres: ')
+  print('\n', hacer_diccionario(entrada), '\n')
+
+  continuar = input('Desea ejecutar de nuevo el programa pulse la tecla S y enter ')
 print ('\n','#'*40,' FIN ', '#'*40)
--- Ejercicio07.py
+++ Ejercicio07.py
@@ -5,17 +5,48 @@ y la cantidad es opcional. Crear los siguientes métodos para la clase:
  Un constructor, donde los datos pueden estar vacíos. 
  Los setters y getters para cada uno de los atributos. El atributo no se puede 
 modificar directamente, sólo ingresando o retirando dinero. 
- mostrar(): Muestra los datos de la cuenta.  ingresar(cantidad): se ingresa 
-una cantidad a la cuenta, si la cantidad introducida es negativa, no se hará nada. 
+ mostrar(): Muestra los datos de la cuenta. 
+ ingresar(cantidad): se ingresa una cantidad a la cuenta, si la cantidad 
+introducida es negativa, no se hará nada. 
  retirar(cantidad): se retira una cantidad a la cuenta. La cuenta puede estar en 
 números rojos.
 '''
+from Ejercicio06 import ingresoPersona
+from Ejercicio06 import Persona
+
+
+class Cuenta(Persona):
+    def __init__(self, cuenta,cantidad):
+          self.cuenta = cuenta
+          self.cantidad=cantidad
+
+    def set_cuenta(self,cuenta):
+         self.cuenta = cuenta
+
+    def get_cuenta(self):
+         return self.cuenta
+    
+def operarCuenta ():
+     total=0
+     operar='n'
+     while operarCuenta!='S' and operar!='s':
+
+          menu= 'Que desea hacer:\n Mostrar los datos de la cuenta presione "M"\n Ingresar dimero en la cuenta pulse "I".\n Retirar dimero en la cuenta pulse "J".\n Salir pulse "S".\n Ingrese su respuesta, verifique y luego presione enter '
+          operar = input(menu)
+          if operar== 'M' or operar== 'm':
+               print(mi_persona, ' ', total,' \n Presione enter para continuar ')
+          if operar== 'I' or operar== 'i':
+               dinero=float(input('Por favor ingrese el monto a depositar: '))
+               total = total + dinero
+          if operar== 'R' or operar== 'r':
+               dinero=float(input('Por favor ingrese el monto a retirar: '))
+               total = total - dinero
+
 print ('@'*40,  'Inicio', '@'*40,'\n')
 continuar= ('s')
-while (continuar=='s' or continuar=='S'):
-
-
-
-
-    continuar = input('Desea ejecutar de nuevo el programa pulse la tecla S y enter ')
+    
+mi_persona=ingresoPersona()
+cuenta=0
+operarCuenta()
+    
 print ('\n','#'*40,' FIN ', '#'*40)
